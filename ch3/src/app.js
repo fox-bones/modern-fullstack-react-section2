a7d606d1613ca3666a0542191b01e3fc2b1083a1
Cayce Harwood <main@Mac.localdomain>
2026-08-27 15:34:01 -0400

    added Express routing

--- ch3/src/app.js
+++ ch3/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import { postsRoutes } from './routes/posts.js'
 
 const app = express()
+app.use(bodyParser.json())
+postsRoutes(app)
 
 app.get('/', (req, res) => {
 	res.send('Hello from Express!')
